1017c9bce58136dca4f8cbd579e703ec61cfb9b0
ditramadia <ditrarizqaamadia@gmail.com>
2022-11-25 15:50:45 +0700

    feat: add try catch rule

---
+++ test/12-tryCatch.js
@@ -0,0 +1,18 @@
+try {
+  if (x == "") {
+    throw "Empty";
+  }
+  if (isNaN(x)) {
+    throw "Not a number";
+  }
+  if (x > 10) {
+    throw "Too high";
+  }
+  if (x < 5) {
+    throw "Too low";
+  }
+} catch (err) {
+  message = "Error";
+} finally {
+  value = "";
+}
