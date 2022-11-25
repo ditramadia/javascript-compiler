48376e8fec8ef8f0f8874fb26bd9c1a4a9c0bd51
ditramadia <ditrarizqaamadia@gmail.com>
2022-11-25 12:10:03 +0700

    feat: add conditional grammar

--- test/5-conditional.js
+++ test/5-conditional.js
@@ -1,12 +1,7 @@
-let get = 0;
-let score = undefined;
-
 if (score < 50) {
   score = "D";
 } else if (score < 65) {
   score = "C";
-} else if (score < 80) {
-  score = "B";
 } else {
   score = "A";
 }
